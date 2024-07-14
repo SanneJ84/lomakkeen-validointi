685b450de14640e14e9bda7bf7ad1f1cefe70db3
Jenna <siitonenjenna@gmail.com>
2024-07-14 12:52:14 +0300

    up

--- lomakkeen_validointi.js
+++ lomakkeen_validointi.js
@@ -2,11 +2,12 @@
 let form = document.getElementById("lomake");                                   
 let error = document.getElementById("error");
 
-document.getElementById("submit", function(event) {
+document.getElementById("submit").addEventListener("click", function(event) {
     event.preventDefault();
+    vahvistalomake();
 });
 
-function vahvistalomake(event) {
+function vahvistalomake() {
 
     let tunnus = document.getElementById("username").value;
     let salasana = document.getElementById("password").value;
@@ -17,20 +18,52 @@ function vahvistalomake(event) {
     let maa = document.getElementById("country").value;
     let gender = document.querySelector('input[name="gender"]:checked');
     let kieli = document.querySelector('input[name="language"]:checked');
-    let pattern = /^(?=.*\d)(?=.*[!@£$€&%#])(?=.*[A-Z]).{6,}$/;
+    let pattern = /^(?=.*\d)(?=.*[!@£$€&%#])(?=.*[A-Z]).{6,}$/;              
 
     // Tyhjennetään mahdolliset virheilmoitukset
     document.getElementById("usernameError").innerText = "";
     document.getElementById("passwordError").innerText = "";
     document.getElementById("emailError").innerText = "";
     document.getElementById("postnumError").innerText = "";
-
+    document.getElementById("nameError").innerText = "";
+    document.getElementById("addressError").innerText = "";
+    document.getElementById("countryError").innerText = "";
+    document.getElementById("genderError").innerText = "";
+    document.getElementById("languageError").innerText = "";
 
     // Tarkistetaan, että kaikki kentät on täytetty
     if (tunnus === "" || salasana === "" || nimi === "" || osoite === "" || postinro === "" || email === "" || maa === "" || !gender || !kieli) {
         error.innerText = "* Täytä kaikki pakolliset kentät";
         console.log("Virheellinen lomake");
         error.style.color = "red";
+
+        if (tunnus === "") {
+            document.getElementById("usernameError").innerText = "*";
+        }
+        if (salasana === "") {
+            document.getElementById("passwordError").innerText = "*";
+        }
+        if (nimi === "") {
+            document.getElementById("nameError").innerText = "*";
+        }
+        if (osoite === "") {
+            document.getElementById("addressError").innerText = "*";
+        }
+        if (postinro === "") {
+            document.getElementById("postnumError").innerText = "*";
+        }
+        if (email === "") {
+            document.getElementById("emailError").innerText = "*";
+        }
+        if (maa === "") {
+            document.getElementById("countryError").innerText = "*";
+        }
+        if (!gender) {
+            document.getElementById("genderError").innerText = "*";
+        }
+        if (!kieli) {
+            document.getElementById("languageError").innerText = "*";
+        }
         return false;
     }
 
@@ -44,26 +77,26 @@ function vahvistalomake(event) {
     }
 
     // Salasanan validointi
-    if (!pattern.test(salasana)) {
+    if (!pattern.test(salasana)) {                  
         error.innerText = "* Tarkista lomake";
         console.log("Salasanan validointi epäonnistui");
-        document.getElementById("passwordError").innerText = "* Salasanan pituus on oltava vähintään 6 merkkiä ja pitää sisällään vähintään yhden numeron, erikoismerkin ja ison kirjaimen";
+        document.getElementById("passwordError").innerText = "* Tunnuksen täytyy sisältää vähintään 6 merkkiä, yksi iso kirjain, yksi numero ja yksi erikoismerkki";
         error.style.color = "red";
         return false;
     }
 
     // Postinumeron validointi
 
-    if (postinro.length !== 5) {
+    if (postinro.length !== 5 || isNaN(postinro)) {
         error.innerText = "* Tarkista lomake";
         console.log("Postinumeron validointi epäonnistui");
         error.style.color = "red";
-        document.getElementById("postnumError").innerText = "* Tarkista postinumero";
+        document.getElementById("postnumError").innerText = "* Postinumeron tulee sisältää 5 numeroa";
         return false;
     }
 
     // Sähköpostin validointi
-    if (!email.includes("@")) {
+    if (!email.includes("@") || email.indexOf(".") < email.indexOf("@") || email.includes(" ")) {
         error.innerText = "* Tarkista lomake";
         console.log("Sähköpostiosoitteen validointi epäonnistui");
         error.style.color = "red";
